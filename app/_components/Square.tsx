7fcc9fa8466fc472c3c06e6bc1c1e6e7b18fe49f
devin-lynch <devin.l.lynch@gmail.com>
2024-02-20 15:29:03 -0600

    Change square bg/border on hover

--- app/_components/Square.tsx
+++ app/_components/Square.tsx
@@ -1,3 +1,5 @@
 export default function Square() {
-  return <div className="size-12 bg-white border-4 border-red-900"></div>;
+  return (
+    <div className="size-12 bg-white border-4 border-red-900 hover:bg-sky-900 hover:border-pink-300"></div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Square from './_components/Square';
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center border-2 border-yellow-300 min-h-screen">
+    <main className="flex justify-center items-center min-h-screen">
       <div className="flex gap-4">
         <Square />
         <Square />
